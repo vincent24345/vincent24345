56a7e7c6c0b1f41507e580a144f82362f801b30a
Vincent Nguyen <vincentn1102@gmail.com>
2024-06-17 13:26:57 +0930

    recursion

---
+++ recursion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    if (n < 2){
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+
+
+int main (){
+    int n = 5;
+    cout << factorial(n) << endl;
+    return 0;
+}
